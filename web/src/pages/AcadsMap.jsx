50450e7baadd7477e7f467014fa9cf2e203b4dff
harshil <harshil.pabbisetti@gmail.com>
2026-02-03 12:17:36 +0530

    fixed cluttering on phone and bugs

--- web/src/pages/AcadsMap.jsx
+++ web/src/pages/AcadsMap.jsx
@@ -1,20 +1,12 @@
 import CampusMap from "./CampusMap";
 
 const AcadsMap = () => {
-  const academicsLocations = [
-    {
-      name: "F Block",
-      position: [17.544826, 78.571054],
-    },
-    {
-      name: "New Acad block",
-      position: [17.545711, 78.570051],
-    },
-    // add more locations
-  ];
-
   return (
-    <CampusMap center={[17.545388, 78.570625]} locations={academicsLocations} />
+    <CampusMap
+      initialCenter={[17.545307, 78.570802]}
+      initialZoom={18}
+      locations={[]}
+    />
   );
 };
 
